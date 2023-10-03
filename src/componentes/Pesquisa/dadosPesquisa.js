adbf76cf24e1892280600f77bada93da8aafe92b
alex-valerio <alexvaleriocontato@gmail.com>
2023-10-02 22:57:04 -0300

    build_0.1

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'BR236214395841S', id: 1, src:livro},
+    {nome: 'BR237666447628J', id: 1, src:livro},
+    {nome: 'BR236262262075K', id: 2, src:livro},
+    {nome: 'BR235624149579K', id: 3, src:livro},
+    {nome: 'BR239614187815O', id: 4, src:livro},
+]
